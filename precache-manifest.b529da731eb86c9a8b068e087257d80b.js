5a853cf47b58a0fc25316db68bba174b315818c6
Rushabh Parekh <rushabh.parekh.11@gmail.com>
2019-09-03 13:23:53 -0400

    Updates

---
+++ precache-manifest.b529da731eb86c9a8b068e087257d80b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d10dfd2def7d588dbc3f99845a9a0ba7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "900b5f7248fdd51e2368",
+    "url": "/static/css/main.bdd2e487.chunk.css"
+  },
+  {
+    "revision": "8a5e799936b2039c25b9",
+    "url": "/static/js/2.f715c4fe.chunk.js"
+  },
+  {
+    "revision": "900b5f7248fdd51e2368",
+    "url": "/static/js/main.5f2721ca.chunk.js"
+  },
+  {
+    "revision": "a8fe4404244fcf07d9ad",
+    "url": "/static/js/runtime~main.7d6fe5c2.js"
+  }
+]);
